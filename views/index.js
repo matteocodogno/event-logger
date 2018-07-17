0c3d59b29a22c2f9488d9f07e82932f804dc8188
Matteo Codogno <matteo.codogno.90@gmail.com>
2018-07-17 22:55:51 +0200

    Customize app header

--- views/index.js
+++ views/index.js
@@ -8,9 +8,31 @@ import HomeScreen from './auth/Home';
 const AppStack = createStackNavigator({
   Home: HomeScreen
   // Other: OtherScreen
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: 'rgb(32,32,32)',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
 });
 
-const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+const AuthStack = createStackNavigator({
+  SignIn: SignInScreen
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: 'rgb(32,32,32)',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
+});
 
 export default createSwitchNavigator(
   {
